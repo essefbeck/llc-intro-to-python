909c3ced0d6838de2039b08e96e817fe68a303f4
deartrudence <trudy.macnabb@gmail.com>
2015-09-14 19:47:12 -0400

    added instruction to run ruby in console

---
+++ hello.rb
@@ -0,0 +1,8 @@
+# Ask user for input
+          # Assign the input value to a variable
+          # Your program waits for the user to hit enter
+          print "Please enter your name: "
+          name = gets.chomp
+
+          # … then continues to this line
+          print("Hello " + name)
