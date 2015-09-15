7c1611436f6be3f70066c35bbc513e445ced48ea
deartrudence <trudy.macnabb@gmail.com>
2015-09-14 20:38:26 -0400

    numbers, comparison and added a slide on string interpolation

--- hello.rb
+++ hello.rb
@@ -1,8 +1,14 @@
 # Ask user for input
           # Assign the input value to a variable
           # Your program waits for the user to hit enter
-          print "Please enter your name: "
-          name = gets.chomp
+          print "What is the weather? "
+          weather = gets.chomp
 
           # … then continues to this line
-          print("Hello " + name)
+          if weather == "cold"
+            print("wear a sweater!")
+          elsif weather == "raining"
+            print("bring your umbrella")
+          else
+            print("dress normally :)")
+          end
