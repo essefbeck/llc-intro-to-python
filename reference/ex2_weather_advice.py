60c6a826ad8ca132871738990fde9a060cb4ad9a
Sarah Beck <sarahbeck@Sarahs-MacBook-Pro.local>
2015-11-07 13:31:37 -0700

    in .rb files adding exercises.

--- reference/ex2_weather_advice.py
+++
@@ -1,9 +0,0 @@
-
-weather = raw_input("What is the weather? (cold, raining, etc.): ")
-
-if weather == "cold":
-    print("Wear a sweater!")
-elif weather == "raining":
-    print("Bring an umbrella")
-else:
-    print("Dress normally :)")
